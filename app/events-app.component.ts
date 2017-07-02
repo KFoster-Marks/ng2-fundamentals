01b6e90a3ade34cc287e777f888d535459bb609b
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2017-07-02 11:44:23 -0600

    Bootstrap app with parent module and component

---
+++ app/events-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app', //custom-html element inserted into html
+  template: '<h2>Hello World</h2>'
+})
+
+export class EventsAppComponent {
+
+}
