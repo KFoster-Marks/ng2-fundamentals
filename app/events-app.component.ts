194bfc988dc4141d25d9c369457d0632e8f9a73d
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2017-07-02 14:07:23 -0600

    Create list item thumbnail component

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app', //custom-html element inserted into html
-  template: '<h2>Hello World</h2>'
+  template: '<events-list></events-list>'
 })
 
 export class EventsAppComponent {
